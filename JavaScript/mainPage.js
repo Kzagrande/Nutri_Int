3800310860a8220e7d9f942a7c2ce0b01cdbdfe6
Yan Casagrande <bortoletoyan@gmail.com.br>
2021-10-28 13:08:37 -0300

    função cálculo gordura corporal

--- JavaScript/mainPage.js
+++ JavaScript/mainPage.js
@@ -10,45 +10,103 @@ var pacientes = document.querySelectorAll(".paciente"); // get id "Julia" and tr
 console.log(pacientes) // printing paciente content
 
 for (var i = 0; i < pacientes.length; i++) {
-  
+
         var paciente = pacientes[i];
 
+
+
+
+        var tdNome = paciente.querySelector(".info-nome");     // get clas "info-peso" and transforming into a variable 
         var tdPeso = paciente.querySelector(".info-peso");     // get clas "info-peso" and transforming into a variable 
         var tdAltura = paciente.querySelector(".info-altura");// get clas "info-altura" and transforming into a variable 
+        var tdGordura = paciente.querySelector(".info-gordura");// get clas "info-altura" and transforming into a variable 
+        var tdSexo = paciente.querySelector(".info-sexo");
+        var tdIdade = paciente.querySelector(".info-idade");
         var tdIMC = paciente.querySelector(".info-imc");// get clas "info-imc" and transforming into a variable 
 
+
+
+
+        var valorNome = tdNome.textContent;      // get a text content of "tdPeso" and asigning to a new variable
         var valorPeso = tdPeso.textContent;      // get a text content of "tdPeso" and asigning to a new variable
         var valorAltura = tdAltura.textContent; // get a text content of "tdAltura" and asigning to a new variable
-        console.log(valorPeso, valorAltura);
+        var valorIdade = tdIdade.textContent;
+        var valorSexo = tdSexo.textContent;
+
+
+
+
+        var tipoMasculino = "Masculino"
+        var tipoFeminino = "Feminino"
+
+        if (valorSexo == tipoMasculino) {
+                valorSexo = 1;
+        }
+        if (valorSexo == tipoFeminino) {
+                valorSexo = 0;
+        }
+        console.log(valorNome, valorPeso, valorAltura, valorIdade, valorSexo);
+
+        var sexoNumero = valorSexo
+
+
 
         var pesoValido = true;
         var alturaValida = true;
+        var idadeValida = true;
+        
+
+
 
         if (valorPeso < 2.0 || valorPeso > 500) {
                 alert("peso menor ou acima do possível ");
                 pesoValido = false;
-                tdIMC.textContent = "peso inválido";
-               // paciente.style.backgroundColor ="lightcoral";
-               paciente.classList.add("infoInvalida")
+                tdPeso.textContent = "peso inválido";
+                // paciente.style.backgroundColor ="lightcoral";
+                paciente.classList.add("infoInvalida")
         }
 
         if (valorAltura < 0.50 || valorAltura > 2.80) {
                 alert("Altura menor ou acima do possível");
                 alturaValida = false;
-                tdIMC.textContent = "altura inválido";
-                paciente.style.backgroundColor ="lightcoral";
-
-        }
-
-        if (pesoValido && alturaValida) {
-                var IMC = valorPeso / (valorAltura * valorAltura); // creating a new variable to define IMC
-                tdIMC.textContent = IMC.toFixed(.5); // editing content of var and asigning your new value is IMC.
-
+                tdAltura.textContent = "altura inválida";
+                paciente.style.backgroundColor = "lightcoral";
         }
 
 
+                if (valorIdade < 1 || valorIdade > 116) {
+                        alert("Idade menor ou acima do possível");
+                        idadeValida = false;
+                        tdIdade.textContent = "Idade inválida";
+                        paciente.style.backgroundColor = "lightcoral";
+                }
 
-}
+                        if (pesoValido && alturaValida) {
+                                var IMC = valorPeso / (valorAltura * valorAltura); // creating a new variable to define IMC
+                                tdIMC.textContent = IMC.toFixed(.5); // editing content of var and asigning your new value is IMC.
+
+
+                        }
+                       
+
+                        
+                        if (idadeValida ) {
+                                var calculoGordura = (1.20 * IMC) + (0.23 * valorIdade) - (10.8 * sexoNumero) - 5.4;// termina depois// cria classe sexo e idade
+                                tdGordura.textContent = calculoGordura.toFixed(.5);
+
+                        } 
+
+                }
+
+        
+
+
+
+
+
+
+
+
 
 
 
---
+++ Readme.md
@@ -0,0 +1,14 @@
+
+
+------------------------------ ⁜NUTR_INT⁜  ------------------------------
+
+
+	A Nutrint é um projeto de um site que simula uma página web para um empresa de nutrição. Que visa utilizando JavaScript fazer uma tabela interativa
+onde o usuário pode adicionar informações básicas do paciente e com base nesses valores informações como IMC e Gordura corporal são calculados automaticamente.
+
+	O projeto ainda está na sua fase inicial e faz parte do meu estudo de JavaScript para Web, conforme for me aprofundando no assunto vou atualizando o 
+mesmo e deixando cada vez mais próximo de uma página profissional.
+
+
+
+				Aguardem por mais atualizações (►__◄) (►__◄)	
